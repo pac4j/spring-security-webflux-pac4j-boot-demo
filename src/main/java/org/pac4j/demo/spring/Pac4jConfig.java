25a92179ad988dc143d8c1b337204152e097e6df
Jérôme LELEU <leleuj@gmail.com>
2022-08-23 14:58:49 +0200

    bridge is autoinstalled

--- src/main/java/org/pac4j/demo/spring/Pac4jConfig.java
+++ src/main/java/org/pac4j/demo/spring/Pac4jConfig.java
@@ -7,12 +7,10 @@ import org.pac4j.core.config.Config;
 import org.pac4j.http.client.direct.DirectBasicAuthClient;
 import org.pac4j.http.credentials.authenticator.test.SimpleTestUsernamePasswordAuthenticator;
 import org.pac4j.oauth.client.TwitterClient;
-import org.pac4j.springframework.security.config.Pac4jSpringSecurityReactiveConfig;
 import org.pac4j.springframework.web.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.server.WebFilter;
 
 import java.util.Optional;
@@ -20,7 +18,6 @@ import java.util.Optional;
 @Configuration
 // to define the callback and logout controllers
 @ComponentScan(basePackages = "org.pac4j.springframework.web")
-@Import(Pac4jSpringSecurityReactiveConfig.class)
 public class Pac4jConfig {
 
     @Bean

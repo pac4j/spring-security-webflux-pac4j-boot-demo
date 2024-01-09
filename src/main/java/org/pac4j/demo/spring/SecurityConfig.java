ddcd7fdf9ead0b75ccab1305fc1470a466f97856
Christian Stein <christian@ververica.com>
2024-01-09 11:27:09 +0100

    Fix SecurityConfig

--- src/main/java/org/pac4j/demo/spring/SecurityConfig.java
+++ src/main/java/org/pac4j/demo/spring/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.pac4j.demo.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,23 +14,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http) {
         return  http
-                .csrf().disable()
-                .authorizeExchange()
-                .pathMatchers("/admin/**").hasRole("ADMIN")
-                .pathMatchers("/user/**").authenticated()
-                .anyExchange().permitAll()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .and().logout()
-                .requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout"))
-                .and().build();
+                .csrf(CsrfSpec::disable)
+                .authorizeExchange(exchanges ->
+                        exchanges.pathMatchers("/admin/**").hasRole("ADMIN")
+                                .pathMatchers("/user/**").authenticated()
+                                .anyExchange().permitAll()
+                )
+                .formLogin(formLogin -> formLogin.loginPage("/login"))
+                .logout(logout ->
+                        logout.requiresLogout(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/logout"))
+                )
+                .build();
     }
 
     @Bean
